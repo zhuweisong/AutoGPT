b8413a4d679cb4e1d5f000230726850bb943633e
SwiftyOS <craigswift13@gmail.com>
2023-09-21 17:47:32 +0200

    Added the write file tutorial example

--- autogpts/forge/forge/agent.py
+++ autogpts/forge/forge/agent.py
@@ -4,13 +4,13 @@ import pprint
 from forge.sdk import (
     Agent,
     AgentDB,
+    ForgeLogger,
+    PromptEngine,
     Step,
     StepRequestBody,
-    Workspace,
-    ForgeLogger,
     Task,
     TaskRequestBody,
-    PromptEngine,
+    Workspace,
     chat_completion_request,
 )
 
@@ -119,26 +119,71 @@ class ForgeAgent(Agent):
         multiple steps. Returning a request to continue in the step output, the user can then decide
         if they want the agent to continue or not.
         """
-        # An example that
-        self.workspace.write(task_id=task_id, path="output.txt", data=b"Washington D.C")
+        # Firstly we get the task this step is for so we can access the task input
+        task = await self.db.get_task(task_id)
+
+        # Create a new step in the database
         step = await self.db.create_step(
             task_id=task_id, input=step_request, is_last=True
         )
-        message = f"\t🔄 Step executed: {step.step_id} input: {step.input[:19]}"
-        if step.is_last:
-            message = (
-                f"\t✅ Final Step completed: {step.step_id} input: {step.input[:19]}"
-            )
 
-        LOG.info(message)
+        # Log the message
+        LOG.info(f"\t✅ Final Step completed: {step.step_id} input: {step.input[:19]}")
 
-        artifact = await self.db.create_artifact(
-            task_id=task_id,
-            step_id=step.step_id,
-            file_name="output.txt",
-            relative_path="",
-            agent_created=True,
+        # Initialize the PromptEngine with the "gpt-3.5-turbo" model
+        prompt_engine = PromptEngine("gpt-3.5-turbo")
+
+        # Load the system and task prompts
+        system_prompt = prompt_engine.load_prompt("system-format")
+
+        # Initialize the messages list with the system prompt
+        messages = [
+            {"role": "system", "content": system_prompt},
+        ]
+
+        # Define the task parameters
+        task_kwargs = {
+            "task": task.input,
+            "abilities": self.abilities.list_abilities_for_prompt(),
+        }
+
+        # Load the task prompt with the defined task parameters
+        task_prompt = prompt_engine.load_prompt("task-step", **task_kwargs)
+
+        # Append the task prompt to the messages list
+        messages.append({"role": "user", "content": task_prompt})
+
+        try:
+            # Define the parameters for the chat completion request
+            chat_completion_kwargs = {
+                "messages": messages,
+                "model": "gpt-3.5-turbo",
+            }
+            # Make the chat completion request and parse the response
+            chat_response = await chat_completion_request(**chat_completion_kwargs)
+            answer = json.loads(chat_response["choices"][0]["message"]["content"])
+
+            # Log the answer for debugging purposes
+            LOG.info(pprint.pformat(answer))
+
+        except json.JSONDecodeError as e:
+            # Handle JSON decoding errors
+            LOG.error(f"Unable to decode chat response: {chat_response}")
+        except Exception as e:
+            # Handle other exceptions
+            LOG.error(f"Unable to generate chat response: {e}")
+
+        # Extract the ability from the answer
+        ability = answer["ability"]
+
+        # Run the ability and get the output
+        # We don't actually use the output in this example
+        output = await self.abilities.run_ability(
+            task_id, ability["name"], **ability["args"]
         )
-        step.output = "Washington D.C"
 
+        # Set the step output to the "speak" part of the answer
+        step.output = answer["thoughts"]["speak"]
+
+        # Return the completed step
         return step
